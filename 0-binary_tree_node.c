0b738002736b183ff046fe431b1de4ae3723fb7d
« Ephely » <ephely1@gmail.com>
2024-12-10 11:01:59 +0100

    trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,25 @@
+#include "binary_trees.h"
+#include <stdio.h>
 
+/** 
+ * binary_tree_node - function that creates a binary tree node
+ * 
+ * @parent: papa & maman
+ * @value: valeur
+ * 
+ * Return: the return
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+    if (new == NULL)
+    {
+        perror("Memory allocation failed");
+        exit(1);
+    }
+    new->data = value;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+}
