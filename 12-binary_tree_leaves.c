65ced02a46b2094fc725bce509b0f952cb19d586
« Ephely » <ephely1@gmail.com>
2024-12-12 14:37:11 +0100

    binary

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,17 +7,19 @@
  *
  * @tree: const
  *
+ * Return: leaves
 */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+
 	if (tree == NULL)
 		return (0);
 
-    if (tree->left == NULL || tree->right == NULL)
+	if (tree->right == NULL && tree->left == NULL)
 		return (1);
 
-	binary_tree_leaves(tree->left);
-	binary_tree_leaves(tree->right);
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
 }
-
